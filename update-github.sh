c9e17d50937296281c7d0c31ef47f0256e04cde2
libramatyas <pancakeboi.sponsorship@gmail.com>
2022-12-01 21:11:57 +0100

    1.0.0

---
+++ update-github.sh
@@ -0,0 +1,13 @@
+#git add --all
+#git commit -m "v$VERSION"
+version=$(cat version.info)
+echo "last known version $version"
+echo "please enter new version id / name (example v1.0.0 or v1.2.3)"
+read -p " > " newver
+echo $newver > version.info
+echo "last known version set to $newver"
+echo "running git..."
+git add --all
+git commit -m "$version"
+git push
+echo "update done."
